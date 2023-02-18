c7632797df3651b0cf499a74c8c96f43bb3bab20
David del Hoyo Carcaboso <davdelcar@alum.us.es>
2023-02-18 17:11:27 +0100

    Position fix while endDraging

--- index.js
+++ index.js
@@ -1,36 +1,45 @@
-function openCart(){
-    const solapaConatainer = document.getElementById('solapa-id')
-    const solapa = document.getElementById('solapa')
-    const carta = document.getElementById('carta');
-    const paper = document.getElementById('paper')
-    solapa.classList.add('swing-top-fwd')
-    carta.classList.remove('bounce-in-top')
-    setTimeout(() => {
-        solapaConatainer.classList.add('solapa-0')
-        solapaConatainer.classList.remove('solapa')
-        paper.classList.add('slide-fwd-top')
-    }, 1000);
-/*     setTimeout(() => {
-        carta.classList.add('slide-out-left')
-    }, 4000); */
-}
-const paper = document.getElementById('paper')
+const solapaConatainer = document.getElementById("solapa-id");
+const solapa = document.getElementById("solapa");
+const carta = document.getElementById("carta");
+const paper = document.getElementById("paper");
 const draggable = document.getElementById("paper");
-      const papel = document.getElementById("paper-container");
-      var offsetX, offsetY;
+const papel = document.getElementById("paper-container");
+function openCart() {
+  solapa.classList.add("swing-top-fwd");
+  carta.classList.remove("bounce-in-top");
+  setTimeout(() => {
+    solapaConatainer.style.zIndex = "0";
+    paper.classList.add("slide-fwd-top");
+  }, 1000);
+}
 
-      function startDrag(event) {
-        papel.style.zIndex = "1000";
-        offsetX = event.touches[0].clientX - draggable.offsetLeft;
-        offsetY = event.touches[0].clientY - draggable.offsetTop;
-      }
+var offsetX, offsetY;
 
-      function drag(event) {
-        event.preventDefault();
-        draggable.style.left = (event.touches[0].clientX - offsetX) + "px";
-        draggable.style.top = (event.touches[0].clientY - offsetY) + "px";
-      }
+function startDrag(event) {
+  papel.style.zIndex = "1000";
+  offsetX = event.touches[0].clientX - draggable.offsetLeft;
+  offsetY = event.touches[0].clientY - draggable.offsetTop;
+  console.log(offsetX);
+  console.log(offsetY);
+}
 
-      function endDrag(event) {
-        // handle end of drag event
-      }
+function drag(event) {
+  event.preventDefault();
+  draggable.style.left = event.touches[0].clientX - offsetX + "px";
+  draggable.style.top = event.touches[0].clientY - offsetY + "px";
+}
+
+function endDrag(event) {
+  const container = document.getElementById("container");
+  draggable.classList.remove("slide-fwd-top");
+  draggable.style.position = "fixed";
+  container.insertBefore(papel, container.firstChild);
+  papel.style.top = "223px";
+  papel.style.bottom = "0px";
+  papel.style.left = "45px";
+  papel.style.right = "0px";
+  
+  setTimeout(() => {
+    carta.classList.add("slide-out-left");
+  }, 1000);
+}
